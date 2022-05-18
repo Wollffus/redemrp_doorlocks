0e8e6b204a217a15ef95f29adcdf826f534e674d
Wollffus <58875783+Wollffus@users.noreply.github.com>
2022-05-18 15:02:19 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,5 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version "bodacious"
 
-fx_version "adamant"
 games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
@@ -15,3 +14,10 @@ client_scripts {
 }
 
 dependency 'redem_roleplay'
+
+escrow_ignore {
+	"config.lua",
+	"readme.md",
+}
+
+lua54 'yes'
